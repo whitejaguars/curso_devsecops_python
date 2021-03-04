6e9bccfe1979c11c6fc7af4ff0707c8529442dfc
Mario Robles <mario.robles@whitejaguars.com>
2021-03-04 01:24:29 -0600

    Added unit tests

---
+++ unit_tests/test_abuse_cases.py
@@ -0,0 +1,9 @@
+class TestAbuseCases():
+    def test_mi_primera_prueba(self):
+        assert True
+
+    def test_mi_segunda_prueba(self):
+        assert True
+
+    def test_mi_tercera_prueba(self):
+        assert True
